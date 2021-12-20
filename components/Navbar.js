f71fe5d6630e2b80680c38237acf0b93686225de
technocalorie <findabirr@gmail.com>
2021-12-20 21:42:37 +0600

    d

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,8 +4,8 @@ function Navbar() {
   return (
     <>
       <nav className="sticky z-50 top-0 shadow-md p-4 bg-gray-200">
-        <div>
-          <img src="/energysolution.svg" alt=""></img>
+        <div className="max-w-8xl mx-auto relative flex items-center">
+          <img src="/energysolution.svg" alt="" className="w-1/5"></img>
         </div>
       </nav>
     </>
