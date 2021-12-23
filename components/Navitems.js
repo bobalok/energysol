3fa43def42d48e84c0c0b25f924a01ab031505f8
technocalorie <findabirr@gmail.com>
2021-12-24 00:44:27 +0600

    blurred dropdown added

--- components/Navitems.js
+++ components/Navitems.js
@@ -47,10 +47,13 @@ export default function Navitems() {
             leaveFrom="transform opacity-100 scale-100"
             leaveTo="transform opacity-0 scale-95"
           >
-            <Menu.Items className="absolute right-0 w-56 mt-2 origin-top-right bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+            <Menu.Items className="absolute right-0 w-56 mt-2 origin-top-right backdrop-blur-sm bg-white/30 divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
               <div className="px-4 py-8 flex flex-col space-y-2">
                 <Menu.Item>
-                  <MyLink href="/Slider" className="hover:text-blue-400">
+                  <MyLink
+                    href="/Slider"
+                    className="hover:text-blue-400 border-4 border-yellow-400"
+                  >
                     Home
                   </MyLink>
                 </Menu.Item>
@@ -64,7 +67,7 @@ export default function Navitems() {
             </Menu.Items>
           </Transition>
         </Menu>
-        <button className="sm:visible text-xl right-0 bg-transparent hover:bg-gradient-to-r from-green-500 to-blue-500 transition-all ease-linear text-blue-400 font-semibold hover:text-white py-4 px-4 border border-blue-400 hover:border-transparent rounded inline-flex items-center">
+        <button className="sm:visible text-xl right-0 bg-transparent hover:bg-gradient-to-r from-green-900 to-red-800 transition-all ease-linear text-blue-400 font-semibold hover:text-white py-4 px-4 border   rounded inline-flex items-center">
           <svg
             className="w-6 h-6"
             fill="currentColor"
