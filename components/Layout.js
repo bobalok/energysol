4ab675ccdb629c2a3892c694f578c3c1825f8830
technocalorie <findabirr@gmail.com>
2022-08-29 18:06:09 +0600

    testing navi

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,11 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import Navi from "./Navi";
 export default function Layout({ children }) {
   return (
     <>
-      <Navbar />
+      <Navi />
+      {/* <Navbar /> */}
       {children}
       <Footer />
     </>
