46b3ef1c5b1b8f0f6ec01725550f5058003e5d6d
technocalorie <findabirr@gmail.com>
2022-09-05 10:24:12 +0600

    formpree script removed

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,11 @@
 import "../styles/globals.css";
-// importing script from nextjs for formsree to work
-import Script from "next/script";
+
 import Layout from "../components/Layout";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
-      <Script src='https://www.google.com/recaptcha/api.js'></Script>
     </Layout>
   );
 }
