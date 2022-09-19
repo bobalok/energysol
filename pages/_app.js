24a64154f42c5b4a15be3ae415e86f8da8d7be85
technocalorie <findabirr@gmail.com>
2022-09-19 13:03:31 +0600

    pageTransition added

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,33 @@
 import "../styles/globals.css";
 import Layout from "../components/Layout";
+import { motion, AnimatePresence } from "framer-motion";
 
 function MyApp({ Component, pageProps, router }) {
   return (
     <Layout>
-      <Component {...pageProps} key={router.route} />
+      <AnimatePresence exitBeforeEnter>
+        <motion.div
+          key={router.route}
+          initial='pageInitial'
+          animate='pageAnimate'
+          exit='pageExit'
+          variants={{
+            pageInitial: {
+              opacity: 0,
+            },
+            pageAnimate: {
+              opacity: 1,
+            },
+            // pageExit: {
+            //   backgroundColor: "white",
+            //   filter: `invert()`,
+            //   opacity: 0,
+            // },
+          }}
+        >
+          <Component {...pageProps} />
+        </motion.div>
+      </AnimatePresence>
     </Layout>
   );
 }
