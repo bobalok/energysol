d984a9717e9b7fd7ede2ae5c50999f96b703bdbc
technocalorie <findabirr@gmail.com>
2022-11-21 18:31:16 +0600

    optimizing

--- components/Sisconcern.js
+++ components/Sisconcern.js
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import Image from "next/legacy/image";
 import Link from "next/link";
 import mdPhoto from "../public/images/person-images/md-portrait.webp";
 import sinamm from "../public/images/partners-logos/comp_sinamm.png";
