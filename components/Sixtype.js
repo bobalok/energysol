9ea8c3bc4c65724deca86a11e12aa90cff9e0251
technocalorie <findabirr@gmail.com>
2023-02-21 12:06:55 +0600

    Made some massive changes to contents in Sixtypejs

--- components/Sixtype.js
+++ components/Sixtype.js
@@ -25,9 +25,9 @@ export default function Sixtype() {
               construction consulting and engineering design for buildings,
               roads, bridges and other infrastructure projects. ESEL’s clients
               include government agencies as well as private sector companies. */}
-              We are good at solving problems. Problems are everywhere. We solve
-              them everyday. We are always looking for ways to improve ourselves
-              and our surroundings.
+              Partnering with us will benefit your business in numerous ways,
+              including increased efficiency, higher customer satisfaction, and
+              improved profitability. Let&rsquo;s grow together!
             </p>
           </div>
 
@@ -42,11 +42,10 @@ export default function Sixtype() {
               <h3 className='mt-4 text-xl font-bold text-white'>Consultancy</h3>
 
               <p className='mt-1 text-base text-gray-300'>
-                We have a team of experienced and skilled people who are helping
-                companies to reach their goals by providing them with best
-                solution they need. We provide consultancy services and These
-                include indentation, market-research, global &amp; local
-                forecasting models and more
+                We excel at consultancy. Our team of experts offers customized
+                solutions to complex business problems. We empower our clients
+                with data-driven insights and proven strategies, enabling them
+                to make informed decisions and achieve long-term success
                 {/* Our team&apos;s vision and mission is to leverage physically
                   in office research, indentation, global forecasting models and
                   expertise to advise government and private companies in
@@ -64,11 +63,10 @@ export default function Sixtype() {
               <h3 className='mt-4 text-xl font-bold text-white'>Supply</h3>
 
               <p className='mt-1 text-base text-gray-300'>
-                Providing supply service from a wide range of suppliable goods
-                that have no specific category. These include specialty
-                chemicals, vehicles, machinery, medical equipment, heavy
-                equipment, materials for power plants, agriculture,
-                construction, and much more
+                We specialize in supplying a diverse range of goods including
+                heavy machinery, vehicles, medical equipment, specialty
+                chemicals, and power plant materials. Count on us for reliable,
+                efficient, and cost-effective procurement services
                 {/* We have been involved in the industry since 2010, we have
                   grown our company from the ground up and now provide
                   everything from materials for power plants, construction,
@@ -89,11 +87,10 @@ export default function Sixtype() {
               </h3>
 
               <p className='mt-1 text-base text-gray-300'>
-                When it comes to construction, it should not only be done fast
-                but also done efficiently. We at “Energy Solution” are here to
-                assist you in achieving both goals. Our team consists of highly
-                skilled professionals who have years of experience in their
-                respective fields
+                Our construction services are designed to meet your needs with
+                exceptional quality, on-time delivery, and within budget. With a
+                strong focus on safety and sustainability, our expert team
+                ensures your project is a success from start to finish
                 {/* We build the future in every project and community.
                   Construction safety management and quality controls, to an
                   extensive network of locations, leading-edge equipments and a
@@ -110,10 +107,10 @@ export default function Sixtype() {
               />
               <h3 className='mt-4 text-xl font-bold text-white'>Engineering</h3>
               <p className='mt-1 text-base text-gray-300'>
-                The team at Engineering is made up of professionals with a
-                favourable national reputation for skill and experience in the
-                design, construction, reconstruction, maintenance and repair of
-                any commercial projects
+                Delivering a wide range of services to meet the needs of our
+                clients. From design and planning to implementation and
+                management, we provide innovative and efficient solutions that
+                drive success. Trust us for all your engineering needs.
                 {/* We offer the services of our professional engineers in the
                   field of project management in executing complex projects. Our
                   Engineers have the experience and track record to drive high-
@@ -132,10 +129,11 @@ export default function Sixtype() {
                 Solar Infrastructure
               </h3>
               <p className='mt-1 text-base text-gray-300'>
-                Creating a better future for ourselves and our children by
-                bringing solar energy to the masses. Solar infrastructures are
-                vital to reduce our reliance on fossil fuels, and empower the
-                new renewable-energy economy
+                We&rsquo;re committed to building a greener future through solar
+                infrastructure. Our team of experts delivers turnkey solutions
+                that drive sustainability, energy efficiency, and cost savings.
+                Join us in creating a better tomorrow by choosing our solar
+                infrastructure services
                 {/* Enhancing the country&apos;s power generation infrastructures
                   to produce more renewable energy as we know Solar energy is
                   widely available and unlimited. No air or water pollution is
@@ -151,13 +149,15 @@ export default function Sixtype() {
                 aria-hidden='true'
               />
               <h3 className='mt-4 text-xl font-bold text-white'>
-                Fertilizer Distribution
+                Fertilizer in bulk
               </h3>
 
               <p className='mt-1 text-base text-gray-300'>
-                We are the largest importer of fertilizer in Bangladesh. We have
-                developed a network for importing fertilizer through sea and air
-                routes, even using container ships when necessary
+                We import different types of fertilizers in bulk, catering to
+                distributors, corporations, and private sectors. Our extensive
+                network of suppliers and logistics management ensures
+                cost-effective and efficient procurement and delivery. Partner
+                with us for reliable and timely supply of quality fertilizers
                 {/* We import best quality fertilizer (in bulk) from around the
                   world. As we know fertilizer is an important input for
                   agriculture production, we’re also a proud member of
