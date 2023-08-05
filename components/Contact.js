5e6dc23e4f37b701a1e4dcfd91d906434e88943d
technocaloire <findabirr@gmail.com>
2023-08-05 15:18:23 +0600

    chat on whatsapp added

--- components/Contact.js
+++ components/Contact.js
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import axios from "axios";
+import WhatsAppButtonGreenMedium from "../public/WhatsAppButtonGreenMedium.svg";
 
 import {
   CheckIcon,
   ChevronDoubleRightIcon,
   ArrowNarrowRightIcon,
 } from "@heroicons/react/solid";
+import Image from "next/image";
 
 export default function Contact() {
   // mail functions goes here
@@ -251,7 +253,7 @@ export default function Contact() {
                   sitekey='6Lf9d8UhAAAAAKl6aMfJMcVHg8Mav6gz1IKwu3BF'
                   onChange={handleChange}
                 /> */}
-                <div className='mt-4'>
+                <div className='mt-4 space-y-4 sm:space-y-0 sm:flex sm:items-center sm:space-x-4'>
                   <button
                     type='submit'
                     disabled={status.submitting}
@@ -296,6 +298,33 @@ export default function Contact() {
                         d='M14 5l7 7m0 0l-7 7m7-7H3'
                       />
                     </svg> */}
+                  </button>
+
+                  <button
+                    className='inline-flex items-center group justify-center w-full px-5 py-3 font-bold text-white bg-green-500 hover:bg-green-600 transition rounded sm:w-auto'
+                    type='button'
+                  >
+                    <a
+                      className='font-bold inline-flex items-center'
+                      aria-label='Chat on WhatsApp'
+                      href='https://wa.me/+8801711591070?text=Obtained%20your%20WhatsApp%20contact%20from%20the%20website.%20Are%20you%20free%20for%20a%20conversation%3F'
+                      target='_blank'
+                      rel='noreferrer'
+                    >
+                      <svg
+                        className='w-6 h-6 mr-2'
+                        fill='currentColor'
+                        viewBox='0 0 30 30'
+                        aria-hidden='true'
+                      >
+                        <path
+                          fillRule='evenodd'
+                          d='M 15 3 C 8.373 3 3 8.373 3 15 C 3 17.251208 3.6323415 19.350068 4.7109375 21.150391 L 3.1074219 27 L 9.0820312 25.431641 C 10.829354 26.425062 12.84649 27 15 27 C 21.627 27 27 21.627 27 15 C 27 8.373 21.627 3 15 3 z M 10.892578 9.4023438 C 11.087578 9.4023438 11.287937 9.4011562 11.460938 9.4101562 C 11.674938 9.4151563 11.907859 9.4308281 12.130859 9.9238281 C 12.395859 10.509828 12.972875 11.979906 13.046875 12.128906 C 13.120875 12.277906 13.173313 12.453437 13.070312 12.648438 C 12.972312 12.848437 12.921344 12.969484 12.777344 13.146484 C 12.628344 13.318484 12.465078 13.532109 12.330078 13.662109 C 12.181078 13.811109 12.027219 13.974484 12.199219 14.271484 C 12.371219 14.568484 12.968563 15.542125 13.851562 16.328125 C 14.986562 17.342125 15.944188 17.653734 16.242188 17.802734 C 16.540187 17.951734 16.712766 17.928516 16.884766 17.728516 C 17.061766 17.533516 17.628125 16.864406 17.828125 16.566406 C 18.023125 16.268406 18.222188 16.319969 18.492188 16.417969 C 18.766188 16.515969 20.227391 17.235766 20.525391 17.384766 C 20.823391 17.533766 21.01875 17.607516 21.09375 17.728516 C 21.17075 17.853516 21.170828 18.448578 20.923828 19.142578 C 20.676828 19.835578 19.463922 20.505734 18.919922 20.552734 C 18.370922 20.603734 17.858562 20.7995 15.351562 19.8125 C 12.327563 18.6215 10.420484 15.524219 10.271484 15.324219 C 10.122484 15.129219 9.0605469 13.713906 9.0605469 12.253906 C 9.0605469 10.788906 9.8286563 10.071437 10.097656 9.7734375 C 10.371656 9.4754375 10.692578 9.4023438 10.892578 9.4023438 z'
+                          clipRule='evenodd'
+                        />
+                      </svg>
+                      Chat on WhatsApp
+                    </a>
                   </button>
                 </div>
               </form>
