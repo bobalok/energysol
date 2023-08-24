f038d0c0ffb0098eeb82423e3dafb462850a0ad3
technocaloire <findabirr@gmail.com>
2023-08-24 16:11:30 +0600

    three company added

--- components/Opportunity.js
+++ components/Opportunity.js
@@ -1,8 +1,11 @@
 import Image from "next/image";
-// import Link from "next/link";
+import Link from "next/link";
 // import mdPhoto from "../public/images/person-images/md-portrait.webp";
-// import sinamm from "../public/images/partners-logos/comp_sinamm.png";
 import handshake from "../public/images/handshakes.jpg";
+import { OfficeBuildingIcon, TagIcon } from "@heroicons/react/outline";
+import sinamm from "../public/images/partners-logos/comp_sinamm_alt.png";
+import sar from "../public/images/partners-logos/comp_sar.png";
+import esgtl from "../public/images/partners-logos/comp_esgtl.png";
 import Contact from "./Contact";
 
 export default function opportunities() {
@@ -10,7 +13,9 @@ export default function opportunities() {
     <section className='max-w-screen-xl px-4 py-16 mx-auto sm:px-6 lg:px-8'>
       <div className='max-w-xl mx-auto text-center'>
         <h2 className='text-3xl font-bold sm:text-4xl'>Sister Concern</h2>
-
+        <p className='mt-4 text-gray-500 sm:text-xl sm:max-w-xl mx-auto text-center'>
+          Always there for each other, no matter what.
+        </p>
         {/* <p className='text-lg text-gray-500 select-none sm:text-justify mt-4'>
           SINAMM ENGINEERING LIMITED named as SINAMM was conceived and formed in
           the year 2004 by a team of qualified Engineers and other professionals
@@ -32,8 +37,353 @@ export default function opportunities() {
         </p>
       </div> */}
 
+      <div className='grid grid-cols-1 gap-8 mt-8 md:grid-cols-3 pb-24'>
+        {/* sar */}
+        <Link href={"#!"}>
+          <a className='block p-4 rounded-lg border shadow-sm shadow-indigo-100 md:hover:-translate-y-1 md:hover:scale-110 md:transition-all'>
+            <div className=''>
+              <Image
+                className='rounded-md'
+                src={sar}
+                alt='sar group'
+                //   height={170}
+                //   width={255}
+                //   layout='intrinsic'
+                //   objectFit='cover'
+                quality='70'
+                // blurDataURL='data:...' // automatically provided
+                placeholder='blur' // Optional blur-up while loading
+              />
+            </div>
+
+            {/* <img
+                alt='123 Wallaby Avenue, Park Road'
+                src='https://images.unsplash.com/photo-1554995207-c18c203602cb'
+                className='object-cover w-full h-56 rounded-md'
+              /> */}
+
+            <div className='mt-2'>
+              <dl>
+                {/* <div>
+                    <dt className='sr-only'>Establishment Date</dt>
+
+                    <dd className='text-sm text-gray-500'>Energy company</dd>
+                  </div> */}
+
+                <div>
+                  <dt className='sr-only'>Company Name</dt>
+
+                  <dd className='font-medium'>SAR GROUP</dd>
+                </div>
+              </dl>
+
+              <dl className='flex items-center mt-6 text-xs space-x-8'>
+                <div className='sm:inline-flex sm:items-center sm:shrink-0'>
+                  {/* <svg
+                      className='w-4 h-4 text-indigo-700'
+                      xmlns='http://www.w3.org/2000/svg'
+                      fill='none'
+                      viewBox='0 0 24 24'
+                      stroke='currentColor'
+                    >
+                      <path
+                        strokeLinecap='round'
+                        strokeLinejoin='round'
+                        strokeWidth='2'
+                        d='M8 14v3m4-3v3m4-3v3M3 21h18M3 10h18M3 7l9-4 9 4M4 10h16v11H4V10z'
+                      />
+                    </svg> */}
+                  <OfficeBuildingIcon className='w-4 h-4 text-gray-500' />
+
+                  <div className='sm:ml-3 mt-1.5 sm:mt-0'>
+                    <dt className='text-gray-500'>Headquarters</dt>
+
+                    <dd className='font-medium'>UAE</dd>
+                  </div>
+                </div>
+
+                <div className='sm:inline-flex sm:items-center sm:shrink-0'>
+                  {/* <svg
+                      className='w-4 h-4 text-indigo-700'
+                      xmlns='http://www.w3.org/2000/svg'
+                      fill='none'
+                      viewBox='0 0 24 24'
+                      stroke='currentColor'
+                    >
+                      <path
+                        strokeLinecap='round'
+                        strokeLinejoin='round'
+                        strokeWidth='2'
+                        d='M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z'
+                      />
+                    </svg> */}
+                  <TagIcon className='w-4 h-4 text-gray-500' />
+
+                  <div className='sm:ml-3 mt-1.5 sm:mt-0'>
+                    <dt className='text-gray-500'>Established </dt>
+
+                    <dd className='font-medium'>2022</dd>
+                  </div>
+                </div>
+
+                {/* <div className='sm:inline-flex sm:items-center sm:shrink-0'>
+                    <svg
+                      className='w-4 h-4 text-indigo-700'
+                      xmlns='http://www.w3.org/2000/svg'
+                      fill='none'
+                      viewBox='0 0 24 24'
+                      stroke='currentColor'
+                    >
+                      <path
+                        strokeLinecap='round'
+                        strokeLinejoin='round'
+                        strokeWidth='2'
+                        d='M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z'
+                      />
+                    </svg>
+
+                    <div className='sm:ml-3 mt-1.5 sm:mt-0'>
+                      <dt className='text-gray-500'>Bedroom</dt>
+
+                      <dd className='font-medium'>4 rooms</dd>
+                    </div>
+                  </div> */}
+              </dl>
+            </div>
+          </a>
+        </Link>
+        <Link href={"#!"}>
+          <a className='block p-4 rounded-lg border shadow-sm shadow-indigo-100 md:hover:-translate-y-1 md:hover:scale-110 md:transition-all'>
+            <div className=''>
+              <Image
+                className='rounded-md'
+                src={sinamm}
+                alt='sinamm engineering'
+                //   height={170}
+                //   width={255}
+                //   layout='intrinsic'
+                //   objectFit='cover'
+                quality='70'
+                // blurDataURL='data:...' // automatically provided
+                placeholder='blur' // Optional blur-up while loading
+              />
+            </div>
+
+            {/* <img
+                alt='123 Wallaby Avenue, Park Road'
+                src='https://images.unsplash.com/photo-1554995207-c18c203602cb'
+                className='object-cover w-full h-56 rounded-md'
+              /> */}
+
+            <div className='mt-2'>
+              <dl>
+                {/* <div>
+                    <dt className='sr-only'>Establishment Date</dt>
+
+                    <dd className='text-sm text-gray-500'>Energy company</dd>
+                  </div> */}
+
+                <div>
+                  <dt className='sr-only'>Company Name</dt>
+
+                  <dd className='font-medium'>Sinamm Engineering Ltd.</dd>
+                </div>
+              </dl>
+
+              <dl className='flex items-center mt-6 text-xs space-x-8'>
+                <div className='sm:inline-flex sm:items-center sm:shrink-0'>
+                  {/* <svg
+                      className='w-4 h-4 text-indigo-700'
+                      xmlns='http://www.w3.org/2000/svg'
+                      fill='none'
+                      viewBox='0 0 24 24'
+                      stroke='currentColor'
+                    >
+                      <path
+                        strokeLinecap='round'
+                        strokeLinejoin='round'
+                        strokeWidth='2'
+                        d='M8 14v3m4-3v3m4-3v3M3 21h18M3 10h18M3 7l9-4 9 4M4 10h16v11H4V10z'
+                      />
+                    </svg> */}
+                  <OfficeBuildingIcon className='w-4 h-4 text-gray-500' />
+
+                  <div className='sm:ml-3 mt-1.5 sm:mt-0'>
+                    <dt className='text-gray-500'>Headquarters</dt>
+
+                    <dd className='font-medium'>Bangladesh</dd>
+                  </div>
+                </div>
+
+                <div className='sm:inline-flex sm:items-center sm:shrink-0'>
+                  {/* <svg
+                      className='w-4 h-4 text-indigo-700'
+                      xmlns='http://www.w3.org/2000/svg'
+                      fill='none'
+                      viewBox='0 0 24 24'
+                      stroke='currentColor'
+                    >
+                      <path
+                        strokeLinecap='round'
+                        strokeLinejoin='round'
+                        strokeWidth='2'
+                        d='M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z'
+                      />
+                    </svg> */}
+                  <TagIcon className='w-4 h-4 text-gray-500' />
+
+                  <div className='sm:ml-3 mt-1.5 sm:mt-0'>
+                    <dt className='text-gray-500'>Established </dt>
+
+                    <dd className='font-medium'>2004 </dd>
+                  </div>
+                </div>
+
+                {/* <div className='sm:inline-flex sm:items-center sm:shrink-0'>
+                    <svg
+                      className='w-4 h-4 text-indigo-700'
+                      xmlns='http://www.w3.org/2000/svg'
+                      fill='none'
+                      viewBox='0 0 24 24'
+                      stroke='currentColor'
+                    >
+                      <path
+                        strokeLinecap='round'
+                        strokeLinejoin='round'
+                        strokeWidth='2'
+                        d='M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z'
+                      />
+                    </svg>
+
+                    <div className='sm:ml-3 mt-1.5 sm:mt-0'>
+                      <dt className='text-gray-500'>Bedroom</dt>
+
+                      <dd className='font-medium'>4 rooms</dd>
+                    </div>
+                  </div> */}
+              </dl>
+            </div>
+          </a>
+        </Link>
+        <Link href={"#!"}>
+          <a className='block p-4 rounded-lg border shadow-sm shadow-indigo-100 md:hover:-translate-y-1 md:hover:scale-110 md:transition-all'>
+            <div className=''>
+              <Image
+                className='rounded-md items-center justify-center'
+                src={esgtl}
+                alt='sinamm engineering'
+                //   height={170}
+                //   width={255}
+                //   layout='intrinsic'
+                //   objectFit='cover'
+                quality='70'
+                // blurDataURL='data:...' // automatically provided
+                placeholder='blur' // Optional blur-up while loading
+              />
+            </div>
+
+            {/* <img
+                alt='123 Wallaby Avenue, Park Road'
+                src='https://images.unsplash.com/photo-1554995207-c18c203602cb'
+                className='object-cover w-full h-56 rounded-md'
+              /> */}
+
+            <div className='mt-2'>
+              <dl>
+                {/* <div>
+                    <dt className='sr-only'>Establishment Date</dt>
+
+                    <dd className='text-sm text-gray-500'>Energy company</dd>
+                  </div> */}
+
+                <div>
+                  <dt className='sr-only'>Company Name</dt>
+
+                  <dd className='font-medium'>
+                    Energy Solutions General Trading L.L.C
+                  </dd>
+                </div>
+              </dl>
+
+              <dl className='flex items-center mt-6 text-xs space-x-8'>
+                <div className='sm:inline-flex sm:items-center sm:shrink-0'>
+                  {/* <svg
+                      className='w-4 h-4 text-indigo-700'
+                      xmlns='http://www.w3.org/2000/svg'
+                      fill='none'
+                      viewBox='0 0 24 24'
+                      stroke='currentColor'
+                    >
+                      <path
+                        strokeLinecap='round'
+                        strokeLinejoin='round'
+                        strokeWidth='2'
+                        d='M8 14v3m4-3v3m4-3v3M3 21h18M3 10h18M3 7l9-4 9 4M4 10h16v11H4V10z'
+                      />
+                    </svg> */}
+                  <OfficeBuildingIcon className='w-4 h-4 text-gray-500' />
+
+                  <div className='sm:ml-3 mt-1.5 sm:mt-0'>
+                    <dt className='text-gray-500'>Headquarters</dt>
+
+                    <dd className='font-medium'>UAE</dd>
+                  </div>
+                </div>
+
+                <div className='sm:inline-flex sm:items-center sm:shrink-0'>
+                  {/* <svg
+                      className='w-4 h-4 text-indigo-700'
+                      xmlns='http://www.w3.org/2000/svg'
+                      fill='none'
+                      viewBox='0 0 24 24'
+                      stroke='currentColor'
+                    >
+                      <path
+                        strokeLinecap='round'
+                        strokeLinejoin='round'
+                        strokeWidth='2'
+                        d='M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z'
+                      />
+                    </svg> */}
+                  <TagIcon className='w-4 h-4 text-gray-500' />
+
+                  <div className='sm:ml-3 mt-1.5 sm:mt-0'>
+                    <dt className='text-gray-500'>Established </dt>
+
+                    <dd className='font-medium'>2012</dd>
+                  </div>
+                </div>
+
+                {/* <div className='sm:inline-flex sm:items-center sm:shrink-0'>
+                    <svg
+                      className='w-4 h-4 text-indigo-700'
+                      xmlns='http://www.w3.org/2000/svg'
+                      fill='none'
+                      viewBox='0 0 24 24'
+                      stroke='currentColor'
+                    >
+                      <path
+                        strokeLinecap='round'
+                        strokeLinejoin='round'
+                        strokeWidth='2'
+                        d='M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z'
+                      />
+                    </svg>
+
+                    <div className='sm:ml-3 mt-1.5 sm:mt-0'>
+                      <dt className='text-gray-500'>Bedroom</dt>
+
+                      <dd className='font-medium'>4 rooms</dd>
+                    </div>
+                  </div> */}
+              </dl>
+            </div>
+          </a>
+        </Link>
+      </div>
+
       {/* Arif Cards starts here */}
-      <div className='max-w-5xl px-4 py-12 mx-auto'>
+      {/* <div className='max-w-5xl px-4 py-12 mx-auto'>
         <section className='p-8 bg-gray-100 rounded-lg md:drop-shadow-xl md:hover:drop-shadow-2xl transition duration-200'>
           <div className='grid grid-cols-1 gap-12 sm:grid-cols-3 sm:items-center'>
             <div className='relative'>
@@ -60,8 +410,6 @@ export default function opportunities() {
                 Sinamm Engineering Ltd:
               </h1>
               <p className='text-xl font-medium sm:text-2xl sm:text-justify'>
-                {/* &quot;We have joined our hands to create a strong hold and take
-                Bangladesh&rsquo;s development to a new horizon.&quot; */}
                 &quot;Our mission is to work hand in hand with our clients to
                 deliver engineering, construction, and related services that
                 make Bangladesh a better place for the present and future
@@ -100,35 +448,11 @@ export default function opportunities() {
                   </svg>
                   <span>LinkedIn</span>
                 </a>
-                {/* <a
-                  className='hover:text-blue-600 flex'
-                  href='https://sinammengineering.com/top-management/'
-                  target='_blank'
-                  rel='noreferrer'
-                >
-                  <span className='sr-only'> Sinamm Engineering Limited</span>
-
-                  <svg
-                    className='w-6 h-6 text-sky-600 mr-1'
-                    fill='none'
-                    stroke='currentColor'
-                    viewBox='0 0 24 24'
-                    xmlns='http://www.w3.org/2000/svg'
-                  >
-                    <path
-                      strokeLinecap='round'
-                      strokeLinejoin='round'
-                      strokeWidth={2}
-                      d='M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13'
-                    />
-                  </svg>
-                  <span>Sinamm&rsquo;s website</span>
-                </a> */}
               </div>
             </blockquote>
           </div>
         </section>
-      </div>
+      </div> */}
 
       <Contact />
     </section>
